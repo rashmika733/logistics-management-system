06fdbc4d6dfa3387071bad9393d4c9c908aba83c
Gihansa555 <rashmikasandheepani@gmail.com>
2025-10-25 01:22:28 +0530

    add new feature add cities

--- main.c
+++ main.c
@@ -7,16 +7,22 @@
 #define MAX_DELIVERIES 50
 #define FUEL_PRICE 310.0
 
+void addCities(int maxCities, char cityNames[][30], int *cityCount);
+
 int main()
 {
     int choice;
     do{
         printf("\n-----LOGISTICS MANAGEMENT SYSTEM-----\n");
+        printf("1.Add new city\n");
         printf("10.Exit\n");
         printf("Enter your choice:");
         scanf("%d",&choice);
 
         switch(choice){
+            case 1:
+           addCities(MAX_CITIES,cityNames,&cityCount);
+           break;
         case 10:
             printf("Exit\n");
             break;
@@ -25,4 +31,25 @@ int main()
         }
         }while(choice!=10);
  return 0;
+}
+void addCities(int maxCities, char cityNames[][30], int *cityCount) {
+
+    char newCity[30];
+    int i = 0;
+
+    if (*cityCount >= maxCities) {
+        printf("\nCannot add more cities. Maximum limit reached.\n");
+        return;
+    }
+    printf("\nEnter city name: ");
+    scanf(" %[^\n]", newCity);
+
+
+    while (newCity[i] != '\0') {
+        cityNames[*cityCount][i] = newCity[i];
+        i++;
+    }
+    cityNames[*cityCount][i] = '\0';
+    (*cityCount)++;
+    printf("City added successfully!\n");
 }
